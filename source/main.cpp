18220622312ba83203ab86f47a08d5bf2c566546
imustend <plochockitymon@gmail.com>
2021-08-06 20:25:33 +0200

    typo fix

--- source/main.cpp
+++ source/main.cpp
@@ -14,101 +14,95 @@
 
 int main(int argc, char *args[])
 {
-	// //declaring shit
-	// SDL_Window *window = nullptr;
-	// SDL_Surface *windowSurface = nullptr;
-	// SDL_Surface *imageSurface = nullptr;
-	// SDL_Surface *playerSurface = nullptr;
+	//declaring shit
+	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *imageSurface = nullptr;
+	SDL_Surface *playerSurface = nullptr;
 
-	// //player pos init
-	// SDL_Rect playerPos;
-	// playerPos.w = 100;
-	// playerPos.h = 100; 
-	// playerPos.x = 0;
-	// playerPos.y = 0;
+	//player pos init
+	SDL_Rect playerPos;
+	playerPos.w = 100;
+	playerPos.h = 100; 
+	playerPos.x = 0;
+	playerPos.y = 0;
 
-	// //initializing window
-	// SDL_Init(SDL_INIT_VIDEO);
+	//initializing window
+	SDL_Init(SDL_INIT_VIDEO);
 
-	// if (SDL_Init(SDL_INIT_AUDIO) < 0)
-	// 	Logger::Error("cant load SDL_AUDIO");
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+		Logger::Error("cant load SDL_AUDIO");
 
-	// Mix_Init(MIX_INIT_MP3);
+	Mix_Init(MIX_INIT_MP3);
 
-	// Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
-	// window = SDL_CreateWindow("fumo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+	Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
+	window = SDL_CreateWindow("fumo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
-	// SDL_Event event;
+	SDL_Event event;
 
-	// //loading images
-	// windowSurface = SDL_GetWindowSurface(window);
-	// imageSurface = SDL_LoadBMP("assets/background_black.bmp");
-	// playerSurface = SDL_LoadBMP("assets/mariss.bmp");
+	//loading images
+	windowSurface = SDL_GetWindowSurface(window);
+	imageSurface = SDL_LoadBMP("assets/background_black.bmp");
+	playerSurface = SDL_LoadBMP("assets/mariss.bmp");
 
 	
-	// Uint32 colorkey = SDL_MapRGB(playerSurface->format, 0xFF, 0x00, 0xFF);
-	// SDL_SetColorKey(playerSurface, SDL_TRUE, colorkey);
+	Uint32 colorkey = SDL_MapRGB(playerSurface->format, 0xFF, 0x00, 0xFF);
+	SDL_SetColorKey(playerSurface, SDL_TRUE, colorkey);
 
-	// //making player with Rect to later blit him into window
-	// // TODO this is stupid and overcomplicated but i dont know how to do it better
-	// while (Game::running)
-	// {
-	// 	while (SDL_PollEvent(&event) != 0)
-	// 	{
-	// 		if (event.type == SDL_QUIT)
-	// 			Game::Stop();
+	//making player with Rect to later blit him into window
+	// TODO this is stupid and overcomplicated but i dont know how to do it better
+	while (Game::running)
+	{
+		while (SDL_PollEvent(&event) != 0)
+		{
+			if (event.type == SDL_QUIT)
+				Game::Stop();
 			
-	// 		Input::Update(event);
-	// 	}
-	// 	//the main loop
+			Input::Update(event);
+		}
+		//the main loop
 
-	// 	//movment stuff
-	// 	float x_vel = (Input::Get(SDLK_d) - Input::Get(SDLK_a));
-	// 	float y_vel = (Input::Get(SDLK_s) - Input::Get(SDLK_w));
+		//movment stuff
+		float x_vel = (Input::Get(SDLK_d) - Input::Get(SDLK_a));
+		float y_vel = (Input::Get(SDLK_s) - Input::Get(SDLK_w));
 
-	// 	float magnitude = sqrt(pow(x_vel, 2) + pow(y_vel, 2));
+		float magnitude = sqrt(pow(x_vel, 2) + pow(y_vel, 2));
 
-	// 	if (magnitude != 0)
-	// 	{
-	// 		playerPos.x += floor((x_vel) / magnitude * SPEED);
-	// 		playerPos.y += floor((y_vel) / magnitude * SPEED);
-	// 	}
+		if (magnitude != 0)
+		{
+			playerPos.x += floor((x_vel) / magnitude * SPEED);
+			playerPos.y += floor((y_vel) / magnitude * SPEED);
+		}
 
-	// 	//collison stuff(with window border)
-	// 	if (playerPos.x <= 0)
-	// 		playerPos.x = 0;
-	// 	if (playerPos.x >= WIDTH - playerPos.w)
-	// 		playerPos.x = WIDTH - playerPos.w;
-	// 	if (playerPos.y <= 0)
-	// 		playerPos.y = 0;
-	// 	if (playerPos.y >= HEIGHT - playerPos.h)
-	// 		playerPos.y = HEIGHT - playerPos.h;
+		//collison stuff(with window border)
+		if (playerPos.x <= 0)
+			playerPos.x = 0;
+		if (playerPos.x >= WIDTH - playerPos.w)
+			playerPos.x = WIDTH - playerPos.w;
+		if (playerPos.y <= 0)
+			playerPos.y = 0;
+		if (playerPos.y >= HEIGHT - playerPos.h)
+			playerPos.y = HEIGHT - playerPos.h;
 
-	// 	// bliting background to window surface 
-	// 	SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
-	// 	// bliting player to background @ playerPos
-	// 	SDL_BlitSurface(playerSurface, NULL, windowSurface, &playerPos);
+		// bliting background to window surface 
+		SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+		// bliting player to background @ playerPos
+		SDL_BlitSurface(playerSurface, NULL, windowSurface, &playerPos);
 
-	// 	SDL_UpdateWindowSurface(window);
-	// 	SDL_Delay(1000 / FPS);
-	// }
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(1000 / FPS);
+	}
 
-	// //memory stuff
-	// SDL_FreeSurface(imageSurface);
-	// imageSurface = nullptr;
-	// SDL_FreeSurface(playerSurface);
-	// playerSurface = nullptr;
-	// SDL_DestroyWindow(window);
-	// window = nullptr;
-	// windowSurface = nullptr;
-	// SDL_Quit();
-	// Mix_Quit();
-
-	Logger::Error("string");
-	Logger::Warn(1.2);
-	Logger::Error(1.2f);
-	Logger::Log(12);
-
+	//memory stuff
+	SDL_FreeSurface(imageSurface);
+	imageSurface = nullptr;
+	SDL_FreeSurface(playerSurface);
+	playerSurface = nullptr;
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	windowSurface = nullptr;
+	SDL_Quit();
+	Mix_Quit();
 
 	return 0;
 }
