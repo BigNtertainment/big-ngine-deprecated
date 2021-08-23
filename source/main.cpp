3dbcba571450ca6ef1481c76fb721c0b885253b2
imustend <plochockitymon@gmail.com>
2021-08-23 04:01:29 +0200

    added render switch on R key

--- source/main.cpp
+++ source/main.cpp
@@ -25,6 +25,11 @@ void processInput(GLFWwindow *window)
 {
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		
 }
 
 
