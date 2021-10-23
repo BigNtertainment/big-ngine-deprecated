f469ff860568be5d6a4b422d94edaa735f715243
Pietrek14 <dawidekpe@gmail.com>
2021-10-23 14:33:26 +0200

    Changed player movement constructors from overloads to default arguments

--- source/behaviours/platformerMovement/platformerMovement.h
+++ source/behaviours/platformerMovement/platformerMovement.h
@@ -5,23 +5,26 @@
 #include "../../global/input/input.h"
 #include <vector>
 
+#define PLATFORMER_MOVEMENT_DEFAULT_SPEED 450.0f
+#define PLATFORMER_MOVEMENT_DEFAULT_JUMP_FORCE 300.0f
+#define PLATFORMER_MOVEMENT_DEFAULT_JUMP_CHECK_PRECISION 5
+#define PLATFORMER_MOVEMENT_DEFAULT_COYOTE_TIME 0.25f
+#define PLATFORMER_MOVEMENT_JUMP_TIMER 0.5f
+
 namespace BigNgine {
 	class PlatformerMovementBehaviour : public Behaviour {
 	public:
 		float speed;
 		float jumpForce;
+		float jumpTimer;
 		int jumpCheckPrecision;
 		float coyoteTime;
 
-		 int LeftButton = BIGNGINE_KEY_A;
-		 int RightButton = BIGNGINE_KEY_D;
-		 int JumpButton = BIGNGINE_KEY_SPACE;
+		int LeftButton = BIGNGINE_KEY_A;
+		int RightButton = BIGNGINE_KEY_D;
+		int JumpButton = BIGNGINE_KEY_SPACE;
 
-		PlatformerMovementBehaviour();
-		PlatformerMovementBehaviour(float _speed);
-		PlatformerMovementBehaviour(float _speed, float _jumpForce);
-		PlatformerMovementBehaviour(float _speed, float _jumpForce, int _jumpCheckPrecision);
-		PlatformerMovementBehaviour(float _speed, float _jumpForce, int _jumpCheckPrecision, float _coyoteTime);
+		PlatformerMovementBehaviour(float _speed = PLATFORMER_MOVEMENT_DEFAULT_SPEED, float _jumpForce = PLATFORMER_MOVEMENT_DEFAULT_JUMP_FORCE, int _jumpCheckPrecision = PLATFORMER_MOVEMENT_DEFAULT_JUMP_CHECK_PRECISION, float _coyoteTime = PLATFORMER_MOVEMENT_DEFAULT_COYOTE_TIME);
 
 		void Start();
 		void Update(int deltaTime);
@@ -29,6 +32,6 @@ namespace BigNgine {
 
 	private:
 		std::vector<PhysicsBehaviour*> physics;
-		float jumpTimer;
+		float _jumpTimer;
 	};
 }
